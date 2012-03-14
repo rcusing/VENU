8b5fa90a8048e15bb0fb17d666058b4df5a0154a
Rachel <rcusing@ucsd.edu>
2012-03-14 11:43:29 -0700

    finalish map

---
+++ finalish/main.js
@@ -0,0 +1,289 @@
+
+//arrays with venue information
+Soma = {name:"Soma",
+        address:"3350 Sports Arena Boulevard",
+        pic:"soma.jpg",
+        website:"http://www.somasandiego.com",
+        gmaps:"http://maps.google.com/maps/place?q=soma+san+diego&cid=14182757072775344695",
+        socialSites:{
+            "Facebook":"http://www.facebook.com/somasd",
+            "Twitter":"http://www.twitter.com/somasd",
+            "MySpace":"http://www.myspace.com/somasd",
+            "Yelp":"http://www.yelp.com/biz/soma-san-diego"},
+        area:"Midway District",
+        scene:"Hip Music Venue",
+        ages:"All ages",
+        tixURL:"https://tickets.somasandiego.com",
+        phone:"619.226.7662"
+};
+HouseofBlues = {name:"House of Blues",
+                address:"1055 5th Avenue",
+                pic:"blues.jpg",
+                website:"http://www.houseofblues.com/sandiego",
+                gmaps:"http://maps.google.com/maps/place?q=house+of+blues+san+diego&hl=en&cid=14113054270661276702",
+                socialSites:{
+                    "Facebook":"http://www.facebook.com/HOBSanDiego",
+                    "Twitter":"http://twitter.com/#!/hobsandiegod",
+                    "MySpace":"http://www.myspace.com/hobsandiego",
+                    "Yelp":"http://www.yelp.com/biz/house-of-blues-san-diego"},
+                area:"Downtown",
+                scene:"Concert Hall",
+                ages:"Ages varies",
+                tixURL:"http://www.livenation.com/House-of-Blues-San-Diego-tickets-San-Diego/venue/82551",
+                phone:"619.299.2583"
+};
+TheLoft = {name:"The Loft at UCSD",
+            address:"Price Center East, 4th Floor, UCSD",
+            pic:"loft.jpg",
+            website:"http://www.theloft.ucsd.edu",
+            gmaps:"http://maps.google.com/maps/place?q=ucsd+the+loft&hl=en&cid=18157079420443764147",
+            socialSites:{
+                "Facebook":"http://www.facebook.com/TheLoftAtUCSD",
+                "Twitter":"http://twitter.com/#!/theloftatucsd",
+                "MySpace":"http://www.myspace.com/theloftucsd",
+                "Yelp":"http://www.yelp.com/biz/the-loft-la-jolla-2"},
+            area:"La Jolla",
+            scene:"Hip Music Venue",
+            ages:"All ages",
+            tixURL:"https://ucsdboxoffice.com/Online/default.asp?doWork::WScontent::loadArticle=Load&BOparam::WScontent::loadArticle::article_id=BC48C4E2-49B4-4B01-89F0-232619F3F746&sessionlanguage=",
+            phone:"858.534.8497"
+};
+Porters = {name:"Porter's Pub at UCSD",
+    address:"Student Center C, UCSD",
+    pic:"porters.jpg",
+    website:"http://www.porterspub.com",
+    gmaps:"http://maps.google.com/maps/place?q=ucsd+porters+pub&hl=en&cid=7528642565884510394",
+    socialSites:{
+        "Facebook":"http://www.facebook.com/PortersPubLaJolla",
+        "Twitter":"http://twitter.com/#!/PortersPub_UCSD",
+        "MySpace":"http://www.myspace.com/porterspub",
+        "Yelp":"http://www.yelp.com/biz/porters-pub-la-jolla"},
+    area:"La Jolla",
+    scene:"Dive Bar w/ Live Music",
+    ages:"All ages",
+    tixURL:"http://www.porterspub.com/index.php",
+    phone:"858.213.3890"
+};
+CheCafe = {name:"The Che Cafe at UCSD",
+            address:"Scholars Drive (off Gilman Dr), UCSD",
+            pic:"che.jpg",
+            website:"http://www.thechecafe.blogspot.com",
+            gmaps:"http://maps.google.com/maps/place?q=Che+Cafe,+CA&hl=en&cid=14603140883765894541",
+            socialSites:{
+                "Facebook":"https://www.facebook.com/che.cafe.collective",
+                "Twitter":"http://twitter.com/#!/thechecafe",
+                "MySpace":"http://www.myspace.com/thechecafe",
+                "Yelp":"http://www.yelp.com/biz/the-che-cafe-collective-la-jolla"},
+            area:"La Jolla",
+            scene:"Historic Music Venue",
+            ages:"All ages",
+            tixURL:"https://tickets.somasandiego.com",
+            phone:"619.226.7662"
+};
+Viejas = {name:"SDSU Viejas Arena and Open Air Theater",
+address:"Aztec Walk (off 55th St), SDSU",
+        pic:"viejas.jpg",
+        website:"http://www.viejasarena.com",
+        gmaps:"http://maps.google.com/maps/place?q=viejas+arena+sdsu&hl=en&cid=5659767995776599249",
+        socialSites:{
+            "Facebook":"https://www.facebook.com/pages/Viejas-Arena/162806547076416",
+            "Twitter":"http://twitter.com/#!/viejas_arena",
+            "MySpace":"#",
+            "Yelp":"http://www.yelp.com/biz/viejas-arena-san-diego"},
+        area:"San Diego State University",
+        scene:"Large Scale Arena",
+        ages:"All ages",
+        tixURL:"http://www.ticketmaster.com/Viejas-Arena-at-Aztec-Bowl-San-Diego-State-University-tickets-San-Diego/venue/82162",
+        phone:"619.594.6947"
+};
+TheCasbah = {name:"The Casbah",
+address:"2501 Kettner Boulevard",
+        pic:"casbah.jpg",
+        website:"http://www.casbahmusic.com",
+        gmaps:"http://maps.google.com/maps/place?q=soma+san+diego&cid=14182757072775344695",
+        socialSites:{
+            "Facebook":"https://www.facebook.com/pages/The-Casbah/141497779244944",
+            "Twitter":"http://twitter.com/#!/casbahsandiego",
+            "MySpace":"http://www.myspace.com/thecasbah",
+            "Yelp":"http://www.yelp.com/biz/the-casbah-san-diego"},
+        area:"Downtown",
+        scene:"Hip Music Venue",
+        ages:"Ages varies",
+        tixURL:"http://casbah.frontgatetickets.com",
+        phone:"619.232.4355"
+};
+RubyRoom = {name:"The Ruby Room",
+address:"1271 University Avenue",
+        pic:"ruby.jpg",
+        website:"http://www.rubyroomsd.com",
+        gmaps:"http://maps.google.com/maps/place?q=ruby+room+san+diego&hl=en&cid=18384368914841913644",
+        socialSites:{
+            "Facebook":"http://www.facebook.com/pages/The-Ruby-Room/108193662289",
+            "Twitter":"http://twitter.com/#!/theRubyRoomSD",
+            "MySpace":"http://www.myspace.com/therubyroomsd",
+            "Yelp":"http://www.yelp.com/biz/ruby-room-san-diego"},
+        area:"Hillcrest",
+        scene:"Dive Bar w/ Live Music",
+        ages:"21+",
+        tixURL:"http://rubyroomsd.com/tickets.php",
+        phone:"619.299.7372"
+};
+ValleyView = {name:"Valley View Casino Center",
+address:"3500 Sports Arena Boulevard",
+        pic:"valley.jpg",
+        website:"http://www.valleyviewcasinocenter.com",
+        gmaps:"http://maps.google.com/maps/place?q=valley+view+casino+center&hl=en&cid=16053275846227861162",
+        socialSites:{
+            "Facebook":"https://www.facebook.com/valleyviewcasinocenter",
+            "Twitter":"http://twitter.com/#!/vvcasinocenter",
+            "MySpace":"#",
+            "Yelp":"http://www.yelp.com/biz/valley-view-casino-center-san-diego-2"},
+        area:"Midway District",
+        scene:"Large Scale Venue",
+        ages:"Ages varies",
+        tixURL:"http://www.ticketmaster.com/Valley-View-Casino-Center-formerly-San-Diego-Sports-Arena-tickets-San-Diego/venue/360477",
+        phone:"619.224.4171"
+};
+Humphreys = {name:"Humphreys Concerts by the Bay",
+address:"2241 Shelter Island Drive",
+        pic:"humphreys.jpg",
+        website:"http://www.humphreysconcerts.com",
+        gmaps:"http://maps.google.com/maps/place?q=Humphrys+Restaurant&hl=en&cid=4738469791999620500",
+        socialSites:{
+            "Facebook":"https://www.facebook.com/groups/35539946488",
+            "Twitter":"http://twitter.com/#!/HumphreysShows",
+            "MySpace":"#",
+            "Yelp":"http://www.yelp.com/biz/humphreys-concerts-by-the-bay-san-diego-3"},
+        area:"Point Loma",
+        scene:"Large Outdoor Theater",
+        ages:"All ages",
+        tixURL:"http://www.ticketmaster.com/Humphreys-Concerts-By-the-Bay-tickets-San-Diego/venue/81964?tm_link=edp_Venue_Name_1",
+        phone:"619.220.8497"
+};
+
+//this function creates the venue info div and places it on the left side of the map area
+function createVenue(array){
+    //create the div object
+    var venue = document.createElement("div");
+    venue.className = "venue";
+    venue.id = array.name;
+
+    //image of venue's main entrance
+    var entrance = document.createElement('img');
+    entrance.src = "images/" + array.pic;
+    entrance.alt = "Image of main entrance";
+
+    //div with list of links to social networking sites with icons
+    var social = document.createElement('div');
+    social.id = "js_social-icons";
+
+    var icons = document.createElement('ul');
+    //this array holds the filenames for each logo, downloaded from http://www.iconfinder.com/search/?q=iconset%3Aumar
+    var iconsImgs = {"Facebook":"fb.png",
+                    "Twitter":"twitter.png",
+                    "MySpace":"myspace.png",
+                    "Yelp":"yelp.png"};
+
+    //this function creates an image hyperlink
+    function imageLink(desc,URL,imgSrc){
+        var link = document.createElement('a');
+        link.title = desc;
+        link.href = URL;
+        link.target = "_blank";
+
+        var logo = document.createElement('img');
+        logo.src = "images/" + imgSrc;
+        link.appendChild(logo);
+
+        return link
+    }
+
+    //create a list item with an image hyperlink for each logo in iconsImgs and add to ul:"icons"
+    for (icon in iconsImgs){
+        socialSite = document.createElement('li');
+        socialLink = imageLink(icon,array.socialSites[icon],iconsImgs[icon]);
+        socialSite.appendChild(socialLink);
+        icons.appendChild(socialSite);
+    };
+
+    social.appendChild(icons);
+
+    //this function converts link text into a helpful description of the link
+    function displayLink(thisDiv){
+        linkText = thisDiv.childNodes[0].childNodes[0];                 //DOM object: TextNode (get the text of the <a> within this <div>)
+        linkLabel = linkText.textContent;                               //the actual string
+
+        var linkURL = linkText.parentNode.attributes[1].textContent;    //URL string
+        if (linkURL.match("maps.google") != null){                      //for the address
+            linkText.textContent = "Go to Google Maps";
+        }
+
+        else {                                                          //for the venue name
+            linkText.textContent = linkURL.slice(11);
+        }
+
+        linkText.parentNode.style.fontStyle = "italic";                 //make it italic
+
+    }
+    //this function reverts link text back to original label
+    function revertText(thisDiv){
+        linkText.textContent = linkLabel;                               //reverts link text to label: name/address
+        linkText.parentNode.style.fontStyle = "normal";                 //no italics
+    }
+
+    //name and link to website
+    var titleDiv = document.createElement('div');
+    titleDiv.id = "name";
+    titleDiv.addEventListener("mouseover",function(){displayLink(this)},false);
+    titleDiv.addEventListener("mouseout",function(){revertText(this)},false);
+
+    var title = document.createElement('a');
+    title.id = "js_venue-title";
+    title.href = array.website;
+    title.target = "_blank";
+    title.textContent = array.name;
+    titleDiv.appendChild(title);
+
+
+    //address and link to google maps
+    var addrDiv = document.createElement('div');
+    addrDiv.id = "address";
+    addrDiv.addEventListener("mouseover",function(){displayLink(this)},false);
+    addrDiv.addEventListener("mouseout",function(){revertText(this)},false);
+
+    var mapsLink = document.createElement('a');
+    mapsLink.id = "js_gmaps";
+    mapsLink.href = array.gmaps;
+    mapsLink.target = "_blank";
+    mapsLink.textContent = array.address;
+    addrDiv.appendChild(mapsLink);
+
+    //this function is for adding additional venue information, i.e. basic chunks of text
+    function addInfo(type,text,id){
+        var name = document.createElement(type);
+        name.textContent = text;
+        name.id = id;
+        return name
+    }
+
+    var area = addInfo("p", "[" + array.area + "]", "area");
+    var scene = addInfo("p", array.scene, "scene");
+    var separator = addInfo("span", "||", "separator");
+    var ages = addInfo("ages", array.ages, "ages");
+    scene.appendChild(separator);
+    scene.appendChild(ages);
+    var boxOfc = addInfo("p", "Box Office: " + array.phone, "phone");
+    var showEvents = addInfo("p", "Show upcoming events", "showEvents");
+
+    //icon downloaded from http://www.iconarchive.com/show/build-icons-by-designkindle/Ticket-icon.html
+    var tix = imageLink("Go to the ticket site",array.tixURL,"ticket.png");
+    tix.id = "tix";
+
+    //array for items to append to venue div
+    venueItems = [entrance, social, titleDiv, addrDiv, area, scene, tix, boxOfc, showEvents]
+    for (item in venueItems){
+        venue.appendChild(venueItems[item])
+    };
+
+    document.getElementById('map-area').appendChild(venue);
+}
